a8120b2877c8a579d5ff47e240f1faa557b271f3
lukasdanckwerth <lukas.danckwerth@gmx.de>
2020-06-30 15:25:18 +0200

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -3,9 +3,7 @@ curl -sSL https://get.docker.com | sh
 
 sudo usermod -aG docker pi
 
-sudo apt-get install -y libffi-dev libssl-dev
-
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y libffi-dev libssl-dev python3 python3-pip
 
 sudo apt-get remove python-configparser
 
